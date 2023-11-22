d2240bc6170f923df72ae0e5d8805416184c46d9
Dariusz Chrubasik <dariuszchrubasik@gmail.com>
2023-11-22 20:40:24 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <table class="table table-bordered table-striped table-hover">
+        <thead>
+        <tr>
+            <th class="left-column fw-bold">Lista filmów</th>
+            <th class="right-column fw-bold">Opcje</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(video, index) in videos" :key="index">
+            <td>{{ video.title }}</td>
+            <td>
+                <button class="btn btn-outline-danger w-100" @click="deleteVideo(index)">Usuń</button>
+            </td>
+        </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+            <td>
+                <input
+                    class="form-control"
+                    type="text"
+                    v-model="videoInput"
+                    placeholder="Dopisz nowy film.."
+                />
+            </td>
+            <td>
+                <button class="btn btn-success w-100" @click="createVideo">Dodaj</button>
+            </td>
+        </tr>
+        </tfoot>
+    </table>
 </template>
 
+<script setup>
+import { ref } from "vue";
+
+const videoInput = ref("");
+const videos = ref([
+    { title: "Głupi i głupszy" }
+]);
+
+const createVideo = () => {
+    if (videoInput.value.trim() !== "") {
+        videos.value.push({ title: videoInput.value });
+        videoInput.value = "";
+    }
+};
+
+const deleteVideo = (index) => {
+    videos.value.splice(index, 1);
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.left-column {
+    width: 100%;
 }
 </style>
